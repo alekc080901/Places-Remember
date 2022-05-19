95c0e21cd977105c19419fdaf927ea251fe3c41d
Alexander <alekc080901@mail.ru>
2022-05-19 22:05:22 +0700

    Add welcome page

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,14 @@
 from . import views
+
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 from django.urls import path
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home, name="home"),
+    path('welcome', views.auth, name="welcome"),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("img/favicon.ico"), permanent=True),
+    ),
 ]
