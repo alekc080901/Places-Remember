62c4f86dbd22a0eadc075c851311872941f36c5f
Alexander <alekc080901@mail.ru>
2022-05-20 16:01:47 +0700

    Finish base website frontend

--- main/views.py
+++ main/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'location_list': [1]})
+
+
+def auth(request):
+    return render(request, 'welcome.html')
