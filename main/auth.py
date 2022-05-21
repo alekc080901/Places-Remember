98e92c54eff06e785f1fe685fe4e3567669e82d2
Alexander <alekc080901@mail.ru>
2022-05-21 12:54:47 +0700

    Website supports authorization

---
+++ main/auth.py
@@ -0,0 +1,45 @@
+import datetime
+
+from django.shortcuts import redirect
+
+
+def is_authenticated(view):
+    def wrapper(request):
+        uid = request.COOKIES.get('user_id')
+        access_token = request.COOKIES.get('access_token')
+        created_at = request.COOKIES.get('created_at')
+        expires_in = request.COOKIES.get('expires_in')
+
+        # Check if the user was authenticated earlier
+        if not all((uid, access_token, created_at, expires_in)):
+            return redirect('/welcome')
+
+        # Token expired validation
+        created_at = datetime.datetime.fromtimestamp(float(created_at))
+        expires_in = datetime.timedelta(seconds=int(expires_in))
+
+        if created_at + expires_in < datetime.datetime.utcnow():
+            return redirect('/welcome')
+
+        return view(request)
+    return wrapper
+
+
+def is_not_authenticated(view):
+    def wrapper(request):
+        uid = request.COOKIES.get('user_id')
+        access_token = request.COOKIES.get('access_token')
+        created_at = request.COOKIES.get('created_at')
+        expires_in = request.COOKIES.get('expires_in')
+
+        # Token expired validation
+        if created_at and expires_in:
+            created_at = datetime.datetime.fromtimestamp(float(created_at))
+            expires_in = datetime.timedelta(seconds=int(expires_in))
+
+        # Check if the user is authenticated
+        if all((uid, access_token, created_at, expires_in)) and created_at + expires_in >= datetime.datetime.utcnow():
+            return redirect('/')
+
+        return view(request)
+    return wrapper
--- main/views.py
+++ main/views.py
@@ -1,9 +1,82 @@
-from django.shortcuts import render
+import datetime
+
+import requests
+
+from django.shortcuts import render, redirect
+
+from control.settings import env
+
+from . import auth
+from .models import User
+from .const import AUTH_ABS_URL
 
 
+@auth.is_authenticated
 def home(request):
-    return render(request, 'home.html', {'location_list': [1]})
+    uid = request.COOKIES.get('user_id')
+    user_info = User.objects.get(uid=uid)
+    full_name = f'{user_info.first_name} {user_info.last_name}'
+
+    context = {
+        'name': full_name,
+        'avatar': user_info.avatar,
+        'location_list': [1],
+    }
+    return render(request, 'home.html', context)
 
 
-def auth(request):
-    return render(request, 'welcome.html')
+@auth.is_not_authenticated
+def welcome(request):
+    context = {
+        'api_id': env('VK_API_ID'),
+        'auth_uri': AUTH_ABS_URL,
+        'page': 'page',
+    }
+    return render(request, 'welcome.html', context)
+
+
+def auth_confirm(request):
+    code = request.GET['code']
+    aid = env('VK_API_ID')
+    secret = env('VK_API_SECRET')
+    redirect_uri = AUTH_ABS_URL
+
+    vk_response = requests.get('https://oauth.vk.com/access_token', params={
+        'client_id': aid,
+        'client_secret': secret,
+        'redirect_uri': redirect_uri,
+        'code': code,
+    })
+    vk_access_content = vk_response.json()
+
+    if not User.objects.filter(uid=vk_access_content['user_id']).exists():
+        user_content = requests.get('https://api.vk.com/method/users.get?user_id=210700286&v=5.131', params={
+            'access_token': env('VK_SECURE_ACCESS_TOKEN'),
+            'user_ids': vk_access_content.get('user_id'),
+            'fields': ['photo_100'],
+            'v': 5.131,
+            'lang': 0,
+        }).json()['response'][0]
+
+        User.objects.create(uid=user_content['id'],
+                            first_name=user_content['first_name'],
+                            last_name=user_content['last_name'],
+                            avatar=user_content['photo_100'],
+                            )
+
+    resp = redirect('/')
+    resp.set_cookie('user_id', vk_access_content['user_id'])
+    resp.set_cookie('access_token', vk_access_content['access_token'])
+    resp.set_cookie('created_at', datetime.datetime.utcnow().timestamp())
+    resp.set_cookie('expires_in', vk_access_content['expires_in'])
+    return resp
+
+
+@auth.is_authenticated
+def logout(request):
+    resp = redirect('/welcome')
+    resp.delete_cookie('user_id')
+    resp.delete_cookie('access_token')
+    resp.delete_cookie('created_at')
+    resp.delete_cookie('expires_in')
+    return resp
